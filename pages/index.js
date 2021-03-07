68ff79b23c225071bb84e8173d556ca4245a0453
davide mezzetti <davide.mezzetti@hotmail.it>
2021-03-07 17:29:12 +0100

    small change to check vercel update on prod

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home () {
         <h1 className={styles.title}>
           Welcome to <a href=''>Space Jelly Shop</a>
         </h1>
+        <p>The best space jellyfish swag in the universe!</p>
 
         <ul className={styles.grid}>
           {products.map(prod => (
@@ -22,14 +23,7 @@ export default function Home () {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
+        <p>Created by Davide Mezzetti</p>
       </footer>
     </div>
   )
