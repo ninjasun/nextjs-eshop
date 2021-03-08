8d3ea8e4da608f5e2c24f5488d164c701d7b93de
davide mezzetti <davide.mezzetti@hotmail.it>
2021-03-08 20:15:49 +0100

    added hooks and cart page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Nav from '../components/nav/Nav'
+import { CartContext, useCartState } from '../hooks/use-cart'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  const cartState = useCartState()
+  return (
+    <CartContext.Provider value={cartState}>
+      <Nav />
+      <Component {...pageProps} />
+    </CartContext.Provider>
+  )
 }
 
 export default MyApp
